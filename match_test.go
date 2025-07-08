e4aa8dcd151ae4caac89776cb65c956c52f7d96e
André devkcud Albanese Junior <patommmmm@proton.me>
2025-07-08 20:25:00 -0300

    refactor(test): remove wrong unit test

--- match_test.go
+++ match_test.go
@@ -1,6 +1,8 @@
 package salvation
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMatcher_CaseExecutesFirstMatch(t *testing.T) {
 	var calls []int
@@ -58,57 +60,19 @@ func TestMatcher_MatchAllCases(t *testing.T) {
 	}
 }
 
-func TestMatcher_DefaultExecutedWhenMatched(t *testing.T) {
-	var defaultCalled bool
-	p := NewPossibility(5)
-
-	p.Match().
-		Case(func(v int) bool { return v > 1 }, func(v int) {}).
-		Default(func(opt Possibly[int]) {
-			defaultCalled = true
-		})
-
-	if !defaultCalled {
-		t.Errorf("expected default to be called when a case matched")
-	}
-}
-
-func TestMatcher_DefaultNotExecutedWhenNotMatched(t *testing.T) {
-	var defaultCalled bool
-	p := NewPossibility(1)
-
-	p.Match().
-		Case(func(v int) bool { return v > 10 }, func(v int) {}).
-		Default(func(opt Possibly[int]) {
-			defaultCalled = true
-		})
-
-	if defaultCalled {
-		t.Errorf("expected default not to be called when no case matched")
-	}
-}
-
 func TestMatcher_WithNothingPossibly(t *testing.T) {
 	var calls []int
-	var defaultCalled bool
 	var ptr *int
 	p := NewPossibility(ptr) // *int nil => Nothing
 
 	p.Match().
 		Case(func(v *int) bool {
-			calls = append(calls, 1)
 			return true
 		}, func(v *int) {
 			calls = append(calls, 10)
-		}).
-		Default(func(opt Possibly[*int]) {
-			defaultCalled = true
 		})
 
 	if len(calls) != 0 {
 		t.Errorf("expected no calls for cases when Possibly is Nothing, got: %v", calls)
-	}
-	if defaultCalled {
-		t.Errorf("expected default not called for Nothing Possibly")
 	}
 }
